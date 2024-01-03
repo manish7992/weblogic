ab278ba0c921f46a537857abe27a3928769aef6c
MANISH KUMAR SINGH <66897947+manish7992@users.noreply.github.com>
2024-01-03 21:38:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,25 +5,28 @@ ADMIN_URL="t3://localhost:7001"
 USERNAME="your_username"
 PASSWORD="your_password"
 WAR_FILE="/path/to/your/app.war"
+APP_NAME="YourApp"  # Replace with your application name
 
 # Function to display script usage
 function display_usage {
-    echo "Usage: $0 -a <Admin_Server_URL> -u <username> -p <password> -w <path_to_war_file>"
+    echo "Usage: $0 -a <Admin_Server_URL> -u <username> -p <password> -w <path_to_war_file> -n <app_name>"
     echo "Options:"
     echo "  -a <Admin_Server_URL>      WebLogic Admin Server URL"
     echo "  -u <username>              WebLogic Admin username"
     echo "  -p <password>              WebLogic Admin password"
     echo "  -w <path_to_war_file>      Path to the WAR file"
+    echo "  -n <app_name>              Name of the application"
     exit 1
 }
 
 # Parse command line options
-while getopts ":a:u:p:w:" opt; do
+while getopts ":a:u:p:w:n:" opt; do
     case $opt in
         a) ADMIN_URL="$OPTARG";;
         u) USERNAME="$OPTARG";;
         p) PASSWORD="$OPTARG";;
         w) WAR_FILE="$OPTARG";;
+        n) APP_NAME="$OPTARG";;
         \?) echo "Invalid option: -$OPTARG" >&2
             display_usage;;
         :) echo "Option -$OPTARG requires an argument." >&2
@@ -32,4 +35,5 @@ while getopts ":a:u:p:w:" opt; do
 done
 
 # Command to redeploy application
-java -cp $MW_HOME/wlserver/server/lib/weblogic.jar weblogic.Deployer -adminurl "$ADMIN_URL" -user "$USERNAME" -password "$PASSWORD" -redeploy "$WAR_FILE"
+/u01/app/jdk/bin/java -cp weblogic.jar weblogic.Deployer -adminurl "$ADMIN_URL" -user "$USERNAME" -password "$PASSWORD" -redeploy -name "$APP_NAME" "$WAR_FILE"
+
